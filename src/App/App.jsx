6f10c1caa826f3bbb2f6206a143ecd8c1e1d9c7c
Aidan McKay <aidanmckay2000@gmail.com>
2019-07-01 17:34:22 -0600

    fetched and display opening crawl
    co-authored-by: Nathan Froehlich <nathan.froeh@gmail.com>

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -7,37 +7,47 @@ class App extends Component {
   constructor() {
     super()
     this.state = {
+      films: [],
       people: [],
       vehicles: [],
       planets: []
     }
   }
   componentDidMount = () => {
-    fetch('https://swapi.co/api/people/')
+    const randomNumber = Math.floor(Math.random() * (6 - 0 + 1))
+    fetch('https://swapi.co/api/films')
       .then(response => response.json())
-      .then(data => this.setState({ people: data.results }))
+      .then(data => this.setState({ films: data.results[randomNumber] }))
       .catch(err => console.log(err))
-    
-    fetch('https://swapi.co/api/vehicles')
-      .then(response => response.json())
-      .then(data => this.setState({ vehicles: data.results }))
-      .catch(err => console.log(err))
-    
-    fetch('https://swapi.co/api/planets')
-      .then(response => response.json())
-      .then(data => this.setState({ planets: data.results}))
-      .catch(err => console.log(err))
-  }
 
+    // fetch('https://swapi.co/api/people/')
+    //   .then(response => response.json())
+    //   .then(data => this.setState({ people: data.results }))
+    //   .catch(err => console.log(err))
+    
+    // fetch('https://swapi.co/api/vehicles')
+    //   .then(response => response.json())
+    //   .then(data => this.setState({ vehicles: data.results }))
+    //   .catch(err => console.log(err))
+    
+    // fetch('https://swapi.co/api/planets')
+    //   .then(response => response.json())
+    //   .then(data => this.setState({ planets: data.results}))
+    //   .catch(err => console.log(err))
+  }
   render() {
     return (
       <div className="App">
-      {console.log(this.state.people, this.state.vehicles, this.state.planets)}
-        <Nav/>
-        <CardComponents/>
+        {this.state.films.opening_crawl}
+        {this.state.films.title}
+        {this.state.films.release_date}
+        <Nav />
+        <CardComponents people={this.state.people} planets={this.state.planets} vehicles={this.state.vehicles}/>
       </div>
     );
   }
 }
+
+
 
 export default App;
