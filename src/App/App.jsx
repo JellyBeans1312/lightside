873407a7afb97fbae52c3bccd848a71eb18fc2a9
Aidan McKay <aidanmckay2000@gmail.com>
2019-07-01 16:22:45 -0600

    fetched data
    co-authored-by: Nathan Froehlich <nathan.froeh@gmail.com>

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,15 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 import CardComponents from '../Card-Components/CardComponents';
 import Nav from '../Nav/Nav'
 import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <Nav/>
-      <CardComponents/>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      people: [],
+      vehicles: [],
+      planets: []
+    }
+  }
+  componentDidMount = () => {
+    fetch('https://swapi.co/api/people/')
+      .then(response => response.json())
+      .then(data => this.setState({ people: data.results }))
+      .catch(err => console.log(err))
+    
+    fetch('https://swapi.co/api/vehicles')
+      .then(response => response.json())
+      .then(data => this.setState({ vehicles: data.results }))
+      .catch(err => console.log(err))
+    
+    fetch('https://swapi.co/api/planets')
+      .then(response => response.json())
+      .then(data => this.setState({ planets: data.results}))
+      .catch(err => console.log(err))
+  }
+
+  render() {
+    return (
+      <div className="App">
+      {console.log(this.state.people, this.state.vehicles, this.state.planets)}
+        <Nav/>
+        <CardComponents/>
+      </div>
+    );
+  }
 }
 
 export default App;
