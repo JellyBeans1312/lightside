140d289cf47711ea845ed4bcd9b01cdde872f9ac
Nathan <nathan.froeh@gmail.com>
2019-07-02 07:20:15 -0600

    Add background image & buttons

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -17,36 +17,36 @@ class App extends Component {
     }
   }
   
-  componentDidMount = () => {
-    const randomNumber = Math.floor(Math.random() * (6 - 0 + 1))
-    setTimeout(() => {
-      this.setState({ isHidden: true })
-    }, 30000);
-    fetch('https://swapi.co/api/films')
-      .then(response => response.json())
-      .then(data => this.setState({ film: data.results[randomNumber] }))
-      .catch(err => console.log(err))
+  // componentDidMount = () => {
+  //   const randomNumber = Math.floor(Math.random() * (6 - 0 + 1))
+  //   setTimeout(() => {
+  //     this.setState({ isHidden: true })
+  //   }, 30000);
+  //   fetch('https://swapi.co/api/films')
+  //     .then(response => response.json())
+  //     .then(data => this.setState({ film: data.results[randomNumber] }))
+  //     .catch(err => console.log(err))
 
-    // fetch('https://swapi.co/api/people/')
-    //   .then(response => response.json())
-    //   .then(data => this.setState({ people: data.results }))
-    //   .catch(err => console.log(err))
+  //   fetch('https://swapi.co/api/people/')
+  //     .then(response => response.json())
+  //     .then(data => this.setState({ people: data.results }))
+  //     .catch(err => console.log(err))
     
-    // fetch('https://swapi.co/api/vehicles')
-    //   .then(response => response.json())
-    //   .then(data => this.setState({ vehicles: data.results }))
-    //   .catch(err => console.log(err))
+  //   fetch('https://swapi.co/api/vehicles')
+  //     .then(response => response.json())
+  //     .then(data => this.setState({ vehicles: data.results }))
+  //     .catch(err => console.log(err))
     
-    // fetch('https://swapi.co/api/planets')
-    //   .then(response => response.json())
-    //   .then(data => this.setState({ planets: data.results}))
-    //   .catch(err => console.log(err))
-  }
+  //   fetch('https://swapi.co/api/planets')
+  //     .then(response => response.json())
+  //     .then(data => this.setState({ planets: data.results}))
+  //     .catch(err => console.log(err))
+  // }
 
   render() {
     return (
       <div className="App">
-        {!this.state.isHidden && <Opening film={this.state.film}/>}
+        {/* {!this.state.isHidden && <Opening film={this.state.film}/>} */}
         <Nav />
         <CardComponents people={this.state.people} planets={this.state.planets} vehicles={this.state.vehicles}/>
       </div>
--- src/Nav/Nav.jsx
+++ src/Nav/Nav.jsx
@@ -3,7 +3,18 @@ import React from 'react';
 
 const Nav = () => {
   return (
-    <div></div>
+    <nav>
+      <h1>Light Side</h1>
+      <div>
+        <button>Favorites</button>
+        <span></span>
+      </div>
+      <div>
+        <button>People</button>
+        <button>Planets</button>
+        <button>Vehicles</button>
+      </div>
+    </nav>
   )
 }
 
