de2493f03eaef86a0e4e13bb8039e2ecaa54edd7
Nathan <nathan.froeh@gmail.com>
2019-07-02 12:23:10 -0600

    Add router

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -3,6 +3,7 @@ import CardComponents from '../Card-Components/CardComponents';
 import Nav from '../Nav/Nav'
 import './App.scss';
 import Opening from '../Opening/Opening';
+import {Route, NavLink } from "react-router-dom";
 
 
 class App extends Component {
@@ -46,11 +47,36 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
+      <main className="App">
         {/* {!this.state.isHidden && <Opening film={this.state.film}/>} */}
-        <Nav />
-        <CardComponents people={this.state.people}/>
-      </div>
+        <nav>
+          <h1>Light Side</h1>
+          <div>
+            <button>Favorites</button>
+            <span></span>
+          </div>
+          <section>
+            <NavLink to={'/'} className='nav'> Home </NavLink>
+            <NavLink to='/people' className='nav'> People </NavLink>
+            <NavLink to='/planets' className='nav'> Planets </NavLink>
+            <NavLink to='/vehicles' className='nav'> Vehicles </NavLink>
+          </section>
+        </nav>
+        {/* <CardComponents people={this.state.people}/> */}
+        {/* <Route exact path='/' component={home}/> */}
+        <Route 
+          exact path='/people' 
+          component={() => <CardComponents people={this.state.people}/>}
+        />
+        <Route 
+          exact path='/planets' 
+          component={() => <CardComponents people={this.state.planets}/>}
+        />
+        <Route 
+          exact path='/vehicles' 
+          component={() => <CardComponents people={this.state.vehicles}/>}
+        />
+      </main>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -3,7 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App/App.jsx';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter} from "react-router-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const router = (
+  <BrowserRouter>
+    <App/>
+  </BrowserRouter>
+)
+
+ReactDOM.render(router, document.getElementById('root'));
 
 serviceWorker.unregister();
