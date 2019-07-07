e65aae7a321f407265715e44f37de61688605851
Nathan <nathan.froeh@gmail.com>
2019-07-07 09:42:47 -0600

    test showCrawl

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Call from '../Fetch/FetchCall'
 import {shallow} from 'enzyme';
 
 
@@ -23,51 +24,21 @@ describe('App', () => {
     expect(wrapper.state('allFavorites').length).toEqual(0)
   })
 
-  it('fetches with the correct link', () => {
-    window.fetch = jest.fn().mockImplementation(() => Promise.resolve({
-      json: () => Promise.resolve(mockData)
-    }))
-    const url = 'https://swapi.co/api/people/'
-    wrapper.instance().getPeople()
-    expect(window.fetch).toHaveBeenCalledWith(url)
+  it('showCrawl should set state', () => {
+    wrapper.setState({film : ''})
+    expect(wrapper.state('film')).toEqual('')
+    wrapper.instance().showCrawl()
+    expect(wrapper.state('film')).toEqual({title:'', opening_crawl:'', release_date:''})
   })
 
-  it('sets the correct state after the fetch finishes', () => {
-    const people = {results: [{
-      "birth_year": "19BBY",
-      "gender": "Male",
-      "eye_color": "blue",
-      "height": "172",
-      "name": "Luke Skywalker",
-      "created": "2014-12-09T13:50:51.644000Z"},
-    {
-      "birth_year": "112BBY",
-      "created": "2014-12-10T15:10:51.357000Z",
-      "eye_color": "yellow",
-      "gender": "n/a",
-      "height": "167",
-      "name": "C-3PO"
-    }]}
-    // const expected = [people]
-    window.fetch = jest.fn().mockImplementationOnce(() => Promise.resolve({
-      ok: true,
-      json: () => Promise.resolve(people)
-    }))
-    // console.log('STATE 1 ', wrapper.state('people'))
-    console.log(wrapper.instance().getPeople())
-    wrapper.instance().getPeople()
-    // .then((res) => console.log(res))
-    .then(() => {
-      expect(wrapper.state('people').length).toEqual(2)
-    })
-    // console.log('STATE 2 ', wrapper.state())
-
-
+  it('showCrawl should call fetchCrawl', () => {
+    const fetch = jest.spyOn(Call, 'fetchCrawl')
+    expect(fetch).not.toHaveBeenCalled()
+    wrapper.instance().showCrawl()
+    expect(fetch).toHaveBeenCalled()
   })
 
-  // it('a fetch error is handled correctly', () => {
-
-  // })
+  
 
 
 
